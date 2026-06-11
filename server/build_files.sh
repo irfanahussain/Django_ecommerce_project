b60c98bea47ce339d7485e6ed3985e15378df611
abc <abc@gmail.com>
2026-06-11 18:50:55 +0530

    Fix python command version

--- server/build_files.sh
+++ server/build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Building project..."
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
 echo "Build complete!"
