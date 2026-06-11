dcb0fd2c68451ea8e12facef09bb5fcf067eb319
abc <abc@gmail.com>
2026-06-11 19:00:46 +0530

    Add STATIC_ROOT for Vercel deployment

--- server/crochetProject/crochetProject/settings.py
+++ server/crochetProject/crochetProject/settings.py
@@ -214,7 +214,7 @@ STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 
-STATIC_URL=BASE_DIR / 'staticfiles'
+STATIC_ROOT=BASE_DIR / 'staticfiles'
 MEDIA_ROOT='static/images'
 
 # Default primary key field type
